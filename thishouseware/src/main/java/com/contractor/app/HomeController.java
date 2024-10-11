0633a24f96069718c1eb7b63bcf31e4476b6c77b
rimtooo <leemtwo@naver.com>
2024-10-11 18:03:43 +0900

    1011

---
+++ thishouseware/src/main/java/com/contractor/app/HomeController.java
@@ -0,0 +1,14 @@
+package com.contractor.app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/* 테스트용 */
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String homePage() {
+		return "home";
+	}
+}

5778c0741db567aa61da384d38dbcac969995605
admin <admin@YD02-06>
2024-10-14 15:28:05 +0900

    1014-권수민

--- thishouseware/src/main/java/com/contractor/app/board/service/PostsVO.java
+++ thishouseware/src/main/java/com/contractor/app/board/service/PostsVO.java
@@ -1,5 +1,19 @@
 package com.contractor.app.board.service;
 
-public class PostsVO {
+import java.util.Date;
 
+import lombok.Data;
+
+@Data
+public class PostsVO {
+	private Integer postsNo; //게시글 번호
+	private Integer boardsNo; //게시판 번호
+	private String postsType; //게시글 유형
+	private String title; //제목
+	private String content; //내용
+	private Integer postSetting; //게시 기간여부
+	private Date postStartTime; //게시 시작시간
+	private Date postEndTime; //게시 종료시간
+	private String authority; //권한
+	private String id; //아이디
 }

07fb025925f86b993e8e1f7ed3781d8cf1c67eb4
rimtooo <leemtwo@naver.com>
2024-10-14 14:58:24 +0900

    1014 yerim 2:58

--- thishouseware/src/main/java/com/contractor/app/company/service/CompanyService.java
+++ thishouseware/src/main/java/com/contractor/app/company/service/CompanyService.java
@@ -1,8 +1,8 @@
-package com.contractor.app.complain.service;
+package com.contractor.app.company.service;
 
 import java.util.List;
 
-public interface ComplainService {
+public interface CompanyService {
 	
 	// 회사(고객)정보 전체조회
 	public List<CompanysVO> companyList();

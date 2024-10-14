36cdf0970842f9bdbd05b957263d8bd9a7aea947
fpsgo7 <lkj4769@naver.com>
2024-10-14 13:58:41 +0900

    manager 관련 파일작성하기

---
+++ thishouseware/src/main/java/com/contractor/app/manager/service/UserVO.java
@@ -0,0 +1,20 @@
+package com.contractor.app.manager.service;
+
+import java.awt.Image;
+import java.util.Date;
+
+public class UserVO {
+	
+	String id;
+	String password;
+	String name;
+	Date hireDt;
+	String phone;
+	String email;
+	String imageLink;
+	Date resignDt;
+	int departemntNo;
+	String positionCode;
+	Image dSignature;
+
+}

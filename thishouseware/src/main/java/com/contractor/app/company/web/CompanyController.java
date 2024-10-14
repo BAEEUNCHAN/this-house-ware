60499a7bc4b7cb011e16032fb845e1d988d16178
rimtooo <leemtwo@naver.com>
2024-10-14 14:58:24 +0900

    1014 yerim 2:58

--- thishouseware/src/main/java/com/contractor/app/company/web/CompanyController.java
+++ thishouseware/src/main/java/com/contractor/app/company/web/CompanyController.java
@@ -1,4 +1,4 @@
-package com.contractor.app.complain.web;
+package com.contractor.app.company.web;
 
 import java.util.List;
 
@@ -7,24 +7,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.contractor.app.complain.service.CompanysVO;
-import com.contractor.app.complain.service.ComplainService;
+import com.contractor.app.company.service.CompanysVO;
+import com.contractor.app.company.service.CompanyService;
 
 @Controller
-public class ComplainController {
-	private ComplainService complainService;
+public class CompanyController {
+	private CompanyService companyService;
 	
 	@Autowired
-	public ComplainController(ComplainService complainService) {
-		this.complainService = complainService;
+	public CompanyController(CompanyService companyService) {
+		this.companyService = companyService;
 	}
 	
 	// 회사(고객)정보 전체조회 : URI - companyLlist / return - complain/companyList
 	@GetMapping("companyList")
 	public String companyList(Model model) {
-		List<CompanysVO>list = complainService.companyList();
+		List<CompanysVO>list = companyService.companyList();
 		model.addAttribute("companys", list);
-		return "complain/companyLIst";
+		return "company/companyLIst";
 	}
 	
 }

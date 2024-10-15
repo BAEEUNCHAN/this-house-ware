bb750538735b8aab287be653b704c653b2e64f3e
BAEEUNCHAN <zy_zy7@naver.com>
2024-10-15 11:30:39 +0900

    leave Servicre 주석처리

--- thishouseware/src/main/java/com/contractor/app/edms/serviceimpl/EdmsServiceImpl.java
+++ thishouseware/src/main/java/com/contractor/app/edms/serviceimpl/EdmsServiceImpl.java
@@ -1,5 +1,32 @@
 package com.contractor.app.edms.serviceimpl;
 
-public class EdmsServiceImpl {
+import java.util.List;
 
-}
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.contractor.app.edms.mapper.EdmsMapper;
+import com.contractor.app.edms.service.EdmsDocVO;
+import com.contractor.app.edms.service.EdmsFormVO;
+import com.contractor.app.edms.service.EdmsService;
+
+@Service
+public class EdmsServiceImpl implements EdmsService {
+	private EdmsMapper edmsMapper;
+
+	@Autowired
+	EdmsServiceImpl(EdmsMapper edmsMapper) {
+		this.edmsMapper = edmsMapper;
+	}
+	
+	@Override
+	public List<EdmsDocVO> edmsDocList() { 
+		return edmsMapper.selectDocAllList();
+	} // 결재문서 전체 출력
+	
+	@Override
+	public List<EdmsFormVO> edmsFormList() { 
+		return edmsMapper.selectFormAllList();
+	} // 결재영식 전체 출력
+
+}// 끝
--- thishouseware/src/main/java/com/contractor/app/leave/service/LeaveService.java
+++ thishouseware/src/main/java/com/contractor/app/leave/service/LeaveService.java
@@ -4,10 +4,12 @@ import java.util.Map;
 
 
 public interface LeaveService {
+	/*
 	// 휴가 신청 등록
 	public String leaveInsert(LeaveVO leaveVO);
 	// 휴가 신청 수정 (반려시)
 	public Map<String, Object> leaveUpdate(LeaveVO leaveVO);
 	// 휴가 신청 삭제 (폐기)
 	public Map<String, Object> leaveDelete(LeaveVO leaveVO);
+	*/
 }

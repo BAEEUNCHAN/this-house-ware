1bb42aa11de6533a1755e2164990fe08a719c755
admin <admin@YD02-06>
2024-10-15 09:51:05 +0900

    VO 생성

--- thishouseware/src/main/java/com/contractor/app/fileroom/service/FileRoomsVO.java
+++ thishouseware/src/main/java/com/contractor/app/fileroom/service/FileRoomsVO.java
@@ -1,5 +1,14 @@
 package com.contractor.app.fileroom.service;
 
-public class FileRoomsVO {
+import lombok.Data;
 
+@Data
+public class FileRoomsVO {
+	private Integer fileRoomsNo; //자료실 번호
+	private String fileRoomsType; //자료실 유형
+	private String fileRoomsName; //자료실 이름
+	private Double capacity; //전체용량
+	private String authority; //권한
+	private String id; //아이디
+	private Double usedCapacity; //사용량
 }

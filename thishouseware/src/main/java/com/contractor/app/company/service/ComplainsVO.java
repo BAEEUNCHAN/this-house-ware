c3b04f5ff8535460c630e2c21b9810bd6386e3d1
rimtooo <leemtwo@naver.com>
2024-10-15 09:50:18 +0900

    1015

--- thishouseware/src/main/java/com/contractor/app/company/service/ComplainsVO.java
+++ thishouseware/src/main/java/com/contractor/app/company/service/ComplainsVO.java
@@ -9,6 +9,7 @@ public class ComplainsVO {
 	private Integer complainNo;		// 문의번호(PK)		, Not Null
 	private Integer complainType;	// 불편구분			, Not Null
 	private Date complainDt;		// 문제발생일시
+	private String complainTitle;   // 문의제목 			, Not Null
 	private String complainContent; // 상황입력			, Not Null
 	private String complainRequest; // 요청사항			, Not Null
 	private Date uploadDt;			// 등록일				, Default sysdate

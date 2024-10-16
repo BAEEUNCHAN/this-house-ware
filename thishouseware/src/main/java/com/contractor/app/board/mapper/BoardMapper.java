dc9e8b4d6a7c3f33695fddaec297065ecc25716e
admin <admin@YD02-06>
2024-10-16 18:02:35 +0900

    1016 - 게시글 작성, 게시글 조회

--- thishouseware/src/main/java/com/contractor/app/board/mapper/BoardMapper.java
+++ thishouseware/src/main/java/com/contractor/app/board/mapper/BoardMapper.java
@@ -3,11 +3,15 @@ package com.contractor.app.board.mapper;
 import java.util.List;
 
 import com.contractor.app.board.service.BoardsVO;
+import com.contractor.app.board.service.PostsVO;
 
 public interface BoardMapper {
-	// 전체조회
-	public List<BoardsVO> selectBoardAll();
-	
-	// 등록
+	// 게시판 전체조회
+	public List<BoardsVO> selectBoardAll(BoardsVO boardsVO);
+
+	// 게시판 등록
 	public int insertBoardInfo(BoardsVO boardsVO);
+
+	// 게시글 전체조회
+	public List<PostsVO> selectPostAll(PostsVO postsVO);
 }
--- thishouseware/src/main/java/com/contractor/app/board/web/BoardController.java
+++ thishouseware/src/main/java/com/contractor/app/board/web/BoardController.java
@@ -1,6 +1,7 @@
 package com.contractor.app.board.web;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,65 +13,72 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.contractor.app.board.service.BoardService;
 import com.contractor.app.board.service.BoardsVO;
 import com.contractor.app.board.service.PostsVO;
+import com.contractor.app.common.service.CommonCodeService;
+import com.contractor.app.common.service.CommonCodeVO;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 	private BoardService boardService;
+	private CommonCodeService commonCodeService;
 	
 	@Autowired
-	public BoardController(BoardService boardService) {
+	public BoardController(BoardService boardService, CommonCodeService commonCodeService) {
 		this.boardService = boardService;
+		this.commonCodeService = commonCodeService;
 	}
-	
+
 	// 메인페이지 : URI - boardMainPage / RETURN - board/boardMainPage
 	@GetMapping("/boardMainPage")
 	public String boardMainPage() {
 		return "board/boardMainPage";
 	}
-	
-	// 전체조회 : URI - boardList / RETURN - board/boardList
+
+	// 게시판 전체조회 : URI - boardList / RETURN - board/boardList
 	@GetMapping("/boardList")
-	public String boardList(Model model) {
-		List<BoardsVO> list = boardService.boardList();
+	public String boardList(Model model,BoardsVO boardsVO) {
+		List<BoardsVO> list = boardService.boardList(boardsVO);
 		// 페이지에 전달
 		model.addAttribute("boards", list);
 		return "board/boardList";
 	}
 	
-	// 등록 - 페이지 : URI - boardInsert / RETURN - board/boardInsert
-	@GetMapping("/boardInsert")
-	public String boardInsertForm() {
-		return "board/boardInsert";
-		// classpath:/templates/board/boardInsert.html => 실제 경로
-	}   
-	    
-	// 등록 - 처리 : URI - boardInsert / PARAMETER - BoardVO(QueryString)
-	//             RETURN - 단건조회 다시 호출
-	@PostMapping("/boardInsert")
-	public String boardInsertProcess(BoardsVO boardsVO) { // <form/> 활용한 submit
-		//int eid = boardService.insertBoard(boardsVO);
-		return "board/boardList";
-		//return "redirect:boardInfo?bno=" + eid;
+	// 게시글 전체조회 : URI - postList / RETURN - board/postList
+	@GetMapping("/postList")
+	public String postList(Model model, PostsVO postsVO) {
+		List<PostsVO> list = boardService.postList(postsVO);
+		// 페이지에 전달
+		model.addAttribute("posts", list);
+		return "board/postList";
 	}
 
 	// 등록 - 페이지 : URI - postInsert / RETURN - board/postInsert
 	@GetMapping("/postInsert")
-	public String postInsertForm(Model model) {
+	public String postInsertForm(Model model, PostsVO postsVO) {
 		// 게시판 목록 조회
-		List<BoardsVO> list = boardService.boardList();
+		List<BoardsVO> list = boardService.boardList(null);
+		
+		// 게시판 유형 조회
+		List<CommonCodeVO> boardsType = commonCodeService.selectCommonCode("0O");
+		
+		// 머리글 유형 조회
+		List<CommonCodeVO> postsType = commonCodeService.selectCommonCode("0P");
+		
 		// 페이지에 전달
+		model.addAttribute("boardsType", boardsType);
 		model.addAttribute("boards", list);
+		model.addAttribute("postsType", postsType);
+		
 		return "board/postInsert";
 		// classpath:/templates/board/postInsert.html => 실제 경로
-	}   
-	
+	}
+
 	// 등록 - 처리 : URI - postInsert / PARAMETER - PostsVO(QueryString)
-	//             RETURN - 단건조회 다시 호출
+	// RETURN - 단건조회 다시 호출
 	@PostMapping("/postInsert")
 	public String postInsertProcess(PostsVO postsVO) { // <form/> 활용한 submit
-		//int eid = boardService.insertPost(postsVO);
+		// int eid = boardService.insertPost(postsVO);
 		return "board/postInsert";
-		//return "redirect:postInfo?pno=" + eid;
+		// return "redirect:postInfo?pno=" + eid;
 	}
 }

7ba2ea72095859fa4ef6dda96f4a4f06b7458573
COM <COM@com001>
2024-10-20 22:05:40 +0900

    1020 예림

--- thishouseware/src/main/java/com/contractor/app/reply/web/ReplyController.java
+++ thishouseware/src/main/java/com/contractor/app/reply/web/ReplyController.java
@@ -1,11 +1,17 @@
 package com.contractor.app.reply.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.contractor.app.company.service.CompanyService;
 import com.contractor.app.complain.service.ComplainService;
+import com.contractor.app.complain.service.ComplainsVO;
 import com.contractor.app.reply.service.ReplyService;
+import com.contractor.app.reply.service.ReplysVO;
 
 @Controller
 public class ReplyController {
@@ -20,9 +26,24 @@ public class ReplyController {
 		this.companyService = companyService;
 	}
 	
+	// 문의 전체조회 : URI - replyList / return - reply/replyList
+	@GetMapping("replyList")
+	public String replyList(ComplainsVO complainVO, Model model) {
+		List<ComplainsVO> list = complainService.complainList();
+		model.addAttribute("complains", list);
+		ComplainsVO findVO = complainService.complainInfo(complainVO);
+		model.addAttribute("complain", findVO);
+		return "reply/replyList";
+	}
 	
-	
-	
+	// reply 단건 + 문의답변
+	@GetMapping("replyInfo")
+	public String replyInfo(ComplainsVO complainVO, Model model, ReplysVO replyVO) {
+		ComplainsVO findVO = complainService.complainInfo(complainVO);
+		// employees 테이블 List / reply + employees 등록
+		model.addAttribute("complain", findVO);
+		return "reply/replyInfo";
+	}
 	
 	
 	

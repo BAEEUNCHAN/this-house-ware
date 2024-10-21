5f827af0771ab7bf60bb88da90a1f8869b20ad26
fpsgo7 <lkj4769@naver.com>
2024-10-21 15:42:23 +0900

    로그인과 로그아웃 구현하기 완료 with 파일명 바꾸기
    SpringSecurityConfig에서는 로그아웃시 이동할 경로를 수정하며 세션을 제거하는 역할을
    추가하였다.
    default_layout : 에는 스프링 시큐리티용 타임리프를 추가하였다.
    default_header : 에는 로그인 하여 스프링 시큐리티 세션에 저장된 정보를 타임리프로 불러와
    출력하는 것을 적용하였다.
    
    pom.xml 충돌 방지를 위해 올리지 않을 것이다.
    		<!-- 버전은 입력하지 않는다. (스프링 부트가 알아서 맞춰준다.)
    		https://mvnrepository.com/artifact/org.thymeleaf.extras/thymeleaf-extras-springsecurity6
    -->
    		<dependency>
    		    <groupId>org.thymeleaf.extras</groupId>
    		    <artifactId>thymeleaf-extras-springsecurity6</artifactId>
    		</dependency>
    가 추가됨

--- thishouseware/src/main/java/com/contractor/app/security/service/impl/CustomUserDetailsService.java
+++ thishouseware/src/main/java/com/contractor/app/security/service/impl/CustomUserDetailsService.java
@@ -14,7 +14,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class CustomerUserDetailsService implements UserDetailsService{
+public class CustomUserDetailsService implements UserDetailsService{
 
 	private final EmployeeMapper empMapper;
 

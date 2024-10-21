009112263b908d7a5e96e116c87e2b99a8bd47db
admin <admin@YD02-06>
2024-10-21 11:41:49 +0900

    1021

--- thishouseware/src/main/java/com/contractor/app/board/web/BoardController.java
+++ thishouseware/src/main/java/com/contractor/app/board/web/BoardController.java
@@ -16,21 +16,36 @@ import com.contractor.app.board.service.PostsVO;
 import com.contractor.app.common.service.CommonCodeService;
 import com.contractor.app.common.service.CommonCodeVO;
 
+import lombok.RequiredArgsConstructor;
+/**
+ * 게시판 관리
+ */
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/board")
 public class BoardController {
-	private BoardService boardService;
-	private CommonCodeService commonCodeService;
+	
+	private final BoardService boardService;
+	private final CommonCodeService commonCodeService;
 
-	@Autowired
-	public BoardController(BoardService boardService, CommonCodeService commonCodeService) {
-		this.boardService = boardService;
-		this.commonCodeService = commonCodeService;
-	}
+//	@Autowired
+//	public BoardController(BoardService boardService, CommonCodeService commonCodeService) {
+//		this.boardService = boardService;
+//		this.commonCodeService = commonCodeService;
+//	}
 
 	// 메인페이지 : URI - boardMainPage / RETURN - board/boardMainPage
 	@GetMapping("/boardMainPage")
-	public String boardMainPage() {
+	public String boardMainPage(Model model, 
+			                    BoardsVO boardsVO, 
+			                    PostsVO postsVO) {
+		// 게시판 목록 조회
+		List<BoardsVO> boards = boardService.selectBoardMain(boardsVO);
+
+		// 페이지에 전달
+		model.addAttribute("boards", boards);
+
+
 		return "board/boardMainPage";
 	}
 
@@ -45,11 +60,33 @@ public class BoardController {
 
 	// 게시글 전체조회 : URI - postList / RETURN - board/postList
 	@GetMapping("/postList")
-	public String postList(Model model, PostsVO postsVO) {
-		List<PostsVO> list = boardService.postList(postsVO);
+	public String postList(Model model, PostsVO postsVO, BoardsVO boardsVO) {
+		BoardsVO board = boardService.selectBoard(boardsVO);
+		List<PostsVO> list = boardService.postListBoard(postsVO);
 		// 페이지에 전달
 		model.addAttribute("posts", list);
+		model.addAttribute("board", board);
 		return "board/postList";
+	}
+
+	// 게시판별 게시글 전체조회 : URI - postListBoard / RETURN - board/postListBoard
+	@GetMapping("/postListBoard")
+	public String postListBoard(Model model, PostsVO postsVO, BoardsVO boardsVO) {
+		// 게시판 목록 조회 - 게시판 이름
+		List<BoardsVO> boards = boardService.boardList(boardsVO);
+
+		// 게시판별 게시글 목록 조회 - 게시글 제목, 게시글 번호
+		List<PostsVO> posts = boardService.postListBoard(postsVO);
+
+		// 머리글 목록 조회
+		List<CommonCodeVO> postsType = commonCodeService.selectCommonCode("0P");
+
+		// 페이지에 전달
+		model.addAttribute("boards", boards);
+		model.addAttribute("posts", posts);
+		model.addAttribute("postsType", postsType);
+		
+		return "board/postListBoard";
 	}
 
 	// 게시글 단건조회 : URI - postInfo / PARAMETER - PostsVO(QueryString)
@@ -57,9 +94,9 @@ public class BoardController {
 	@GetMapping("postInfo") // postInfo?key=value
 	public String postInfo(PostsVO postsVO, Model model) {
 		PostsVO findVO = boardService.postInfo(postsVO);
-		
+
 		model.addAttribute("post", findVO);
-		
+
 		return "board/postInfo";
 		// classpath:/templates/board/postInfo.html => 실제 경로
 	}
@@ -96,12 +133,13 @@ public class BoardController {
 		int postsNo = boardService.insertPost(postsVO);
 		return "redirect:postInfo?postsNo=" + postsNo;
 	}
-	
+
 	// 게시글 삭제 - 처리 : URI - postDelete / PARAMETER - Integer
-    //             RETURN - 전체조회 다시 호출
+	// RETURN - 전체조회 다시 호출
 	@GetMapping("/postDelete") // QueryString : @RequestParam
-	public String postDelete(@RequestParam Integer postsNo) {
+	public String postDelete(@RequestParam Integer postsNo, 
+			                 @RequestParam Integer boardsNo) {
 		boardService.deletePost(postsNo);
-		return "redirect:postList";
+		return "redirect:postList?boardsNo=" + boardsNo;
 	}
 }

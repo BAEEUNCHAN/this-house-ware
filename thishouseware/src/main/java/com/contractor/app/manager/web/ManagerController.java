7ce9ba51f9340cf29c4d74008fd969dc1ee10654
fpsgo7 <lkj4769@naver.com>
2024-10-21 09:57:30 +0900

    파일업로드 기능 static으로 꺼내어 공통으로 사용할 수 있게하기 성공
    FileUpload : 파일 업로드 하는 메서드들이 있으며 public static 이라 자유롭게 접근하여 사용할
    수 있다.
    
    ManagerController : 해당 FileUpload 클래스의 메서드를 사용한느 컨트롤러이다.
    사용하는 예시로서 참고 할 수 있다.

--- thishouseware/src/main/java/com/contractor/app/manager/web/ManagerController.java
+++ thishouseware/src/main/java/com/contractor/app/manager/web/ManagerController.java
@@ -26,6 +26,7 @@ import com.contractor.app.employee.EmployeeUtil;
 import com.contractor.app.employee.service.DepartmentVO;
 import com.contractor.app.employee.service.EmployeeService;
 import com.contractor.app.employee.service.EmployeeVO;
+import com.contractor.app.util.FileUpload;
 
 import groovy.util.logging.Slf4j;
 import lombok.RequiredArgsConstructor;
@@ -100,32 +101,12 @@ public class ManagerController {
 	    	answer = "error1";
 	    	return answer;
 	    }
-	    
-        String fileName = uploadFile.getOriginalFilename();
-        // System.out.println("fileName : " + fileName);
-        // 충돌방지를 위해 날짜 폴더와 UUID를 통한 고유식별자를 활용한다.
-        //날짜 폴더 생성
-        String folderPath = makeFolder();
-        //UUID 고유 식별자를통해 중복 방지 
-        String uuid = UUID.randomUUID().toString();
-        //저장할 파일 이름 중간에 "_"를 이용하여 구분
-        String uploadFileName = folderPath +File.separator + uuid + "_" + fileName;
-        String saveName = uploadPath + File.separator + uploadFileName;
-        //Paths.get() 메서드는 특정 경로의 파일 정보를 가져옵니다.(경로 정의하기)
-        Path savePath = Paths.get(saveName);
-        System.out.println("path : " + saveName);
-        try{
-        	//uploadFile에 파일을 업로드 하는 메서드 transferTo(file)
-        	uploadFile.transferTo(savePath);  
-        } catch (IOException e) {
-        	answer = "error2";
-            return answer;        
-        }
-        
-        // DB 에 저장하기위한 파일의 경로는 추출한다.
-        imageLink = setImagePath(uploadFileName);
+        System.out.println(uploadPath);
+        // DB 에 저장하기위한 파일의 경로 추출과 파일 저장을 동시에한다.
+        imageLink = FileUpload.fileUpload(uploadFile,"img/emp/", uploadPath);
+        if(imageLink == null)
+        	return "error2";
         empVO.setImageLink(imageLink);
-        System.out.println(empVO);
         
         // 서버입력 성공 여부
         try {
@@ -148,10 +129,9 @@ public class ManagerController {
 			@RequestPart(required = false) MultipartFile uploadFile ) {
 
 		String answer = null;
-	    String uploadFileName = null;
+	    String imageLink = null;
 	    if(uploadFile !=null) {
-	    	// 기존 파일 삭제하기
-	        System.out.println(deleteFile(empVO.getImageLink()));
+	    	String beforeImageLink = empVO.getImageLink(); // 기존에 사용하던 이미지 링크
 	    	
 	    	// 이미지 파일이 아니면은 처리하지 않는다.(콘텐츠 타입으로 확인)
 		    if(uploadFile.getContentType().startsWith("image") == false){
@@ -160,25 +140,16 @@ public class ManagerController {
 		    	return answer;
 		    }
 		    
-	    	String fileName = uploadFile.getOriginalFilename();
-	        String folderPath = makeFolder();
-	        String uuid = UUID.randomUUID().toString();
-	        uploadFileName = folderPath +File.separator + uuid + "_" + fileName;
-	        String saveName = uploadPath + File.separator + uploadFileName;
-	        Path savePath = Paths.get(saveName);
-	        System.out.println("path : " + saveName);
-	        try{
-	        	uploadFile.transferTo(savePath);  
-	        } catch (IOException e) {
-	        	answer = "error2";
-	            return answer;        
+		    // DB 에 저장하기위한 파일의 경로는 추출한다.
+		    imageLink = FileUpload.fileUpload(uploadFile,"img/emp/" ,uploadPath);
+	        if(imageLink ==null) {
+	        	return "error2";
 	        }
+	        empVO.setImageLink(imageLink);
+	        
+	        // 기존 파일 삭제하기
+	        System.out.println(FileUpload.deleteFile(beforeImageLink,uploadPath));
 	    }
-        
-        // DB 에 저장하기위한 파일의 경로는 추출한다.
-        if(uploadFileName !=null) {
-        	empVO.setImageLink(setImagePath(uploadFileName));
-        }
         
         // 서버입력 성공 여부
         try {
@@ -190,48 +161,5 @@ public class ManagerController {
 	    answer = empVO.getId();
 	    return answer;
 	}
-	
-	private String deleteFile(String beforeImageLink){
-		
-		String filePath = uploadPath + beforeImageLink;
-	    File file = new File(filePath);	
-	    if(file.exists()) {
-	    	if(file.delete()){
-	    		return "파일삭제 성공";
-    		}else{
-    			return "파일삭제 실패";
-    		}
-	    }
-	    return "파일이 없습니다.";
-	}
-
-	/**
-	 * 현재 날짜 값을 토대로 폴더를 만들어주는 메서드이다.
-	 * @return 폴더 경로 내용
-	 */
-	private String makeFolder() {
-		String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
-		String folderPath = "img/emp/"; // img를 저장할 것이기에 img를 추가한다.
-		// LocalDate를 문자열로 포멧
-		folderPath += str.replace("/", File.separator);
-		File uploadPathFoler = new File(uploadPath, folderPath);
-		
-		// File newFile= new File(dir,"파일명");
-		if (uploadPathFoler.exists() == false) {
-			uploadPathFoler.mkdirs();
-			// 만약 uploadPathFolder가 존재하지않는다면 makeDirectory하라는 의미입니다.
-			// mkdir(): 디렉토리에 상위 디렉토리가 존재하지 않을경우에는 생성이 불가능한 함수
-			// mkdirs(): 디렉토리의 상위 디렉토리가 존재하지 않을 경우에는 상위 디렉토리까지 모두 생성하는 함수
-		}
-		return folderPath;
-	}
-	
-	/**
-	 * 파일이 저장되는 경로를 구해준다.
-	 */
-	private String setImagePath(String uploadFileName) {
-		return uploadFileName.replace(File.separator, "/");
-	}
-
 
 }

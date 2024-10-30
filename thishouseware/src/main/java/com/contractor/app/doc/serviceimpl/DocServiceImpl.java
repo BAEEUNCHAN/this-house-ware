3963912bde092f51dd4dc1f18d3ebef421a2faed
BAEEUNCHAN <zy_zy7@naver.com>
2024-10-30 16:34:54 +0900

    1030 머지 전 커밋

--- thishouseware/src/main/java/com/contractor/app/doc/serviceimpl/DocServiceImpl.java
+++ thishouseware/src/main/java/com/contractor/app/doc/serviceimpl/DocServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.contractor.app.doc.mapper.DocMapper;
 import com.contractor.app.doc.service.DocJoinVO;
 import com.contractor.app.doc.service.DocService;
+import com.contractor.app.util.DepartmentNameUtil;
 
 @Service
 public class DocServiceImpl implements DocService {
@@ -20,32 +21,62 @@ public class DocServiceImpl implements DocService {
 
 	@Override
 	public List<DocJoinVO> DocJoinList(String id) {
-		return docMapper.selectDocJoinList(id);
-	} // 문서함 문서 전체조회	
+		List<DocJoinVO> list = docMapper.selectDocJoinList(id);
+		/*
+		 * for (DocJoinVO docJoinVO : list) {
+		 * docJoinVO.setDepartmentName(DepartmentNameUtil.getDepartmentName(docJoinVO.
+		 * getDepartmentNo())); }
+		 */
+		for (int i = 0; i < list.size(); i++) {
+			int deptNo = list.get(i).getDepartmentNo();
+			list.get(i).setDepartmentName(DepartmentNameUtil.getDepartmentName(deptNo));
+		}
+		return list;
+	} // 문서함 문서 전체조회
 
 	@Override
-	public List<DocJoinVO> docApprovalStatusList(String approvalStatus,String id) {
-		return docMapper.getApprovalStatus(approvalStatus,id);
-	} // 문서결과별 문서 조회	
+	public List<DocJoinVO> docApprovalStatusList(String approvalStatus, String id) {
+		List<DocJoinVO> list = docMapper.getApprovalStatus(approvalStatus, id);
+		for (int i = 0; i < list.size(); i++) {
+			int deptNo = list.get(i).getDepartmentNo();
+			list.get(i).setDepartmentName(DepartmentNameUtil.getDepartmentName(deptNo));
+		}
+		return list;
+	} // 문서결과별 문서 조회
 
 	@Override
 	public List<DocJoinVO> DocDeptList(int departmentNo) {
-		return docMapper.selectDocDeptList(departmentNo);
+		List<DocJoinVO> list = docMapper.selectDocDeptList(departmentNo);
+		for (int i = 0; i < list.size(); i++) {
+			int deptNo = list.get(i).getDepartmentNo();
+			list.get(i).setDepartmentName(DepartmentNameUtil.getDepartmentName(deptNo));
+		}
+		return list;
 	} // 부서별 문서 전체조회
-	
+
 	@Override
 	public List<DocJoinVO> docDeptStatusList(String approvalStatus, int departmentNo) {
-	    return docMapper.getDeptStatus(approvalStatus, departmentNo); 
+		List<DocJoinVO> list = docMapper.getDeptStatus(approvalStatus, departmentNo);
+		for (int i = 0; i < list.size(); i++) {
+			int deptNo = list.get(i).getDepartmentNo();
+			list.get(i).setDepartmentName(DepartmentNameUtil.getDepartmentName(deptNo));
+		}
+		return list;
 	} // 부서문서 결재완료 조회
-	
+
 	@Override
 	public void docUpdateImportant(String edmsDocNo, String important) {
-			docMapper.updateDeptImportant(edmsDocNo,important);
+		docMapper.updateDeptImportant(edmsDocNo, important);
 	} // 부서문서 중요문서 업데이트
-	
+
 	@Override
 	public List<DocJoinVO> docDeptImportantList(String important, int departmentNo) {
-		return docMapper.getDeptImportant(important, departmentNo);
+		List<DocJoinVO> list = docMapper.getDeptImportant(important, departmentNo);
+		for (int i = 0; i < list.size(); i++) {
+			int deptNo = list.get(i).getDepartmentNo();
+			list.get(i).setDepartmentName(DepartmentNameUtil.getDepartmentName(deptNo));
+		}
+		return list;
 	} // 부서문서 중요문서
-	
+
 }// 끝

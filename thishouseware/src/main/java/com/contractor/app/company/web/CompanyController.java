f5360e56ff153dda7f9357f11afe6d82a09453e2
rimtooo <leemtwo@naver.com>
2024-11-07 17:50:54 +0900

    1107 예림

--- thishouseware/src/main/java/com/contractor/app/company/web/CompanyController.java
+++ thishouseware/src/main/java/com/contractor/app/company/web/CompanyController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.contractor.app.company.service.CompanyService;
 import com.contractor.app.company.service.CompanysVO;
@@ -30,6 +31,21 @@ public class CompanyController {
 		model.addAttribute("companys", list);
 		return "company/companyList";
 	}
+	
+	// 회사정보 등록 처리
+	@PostMapping("company/companyList")
+	public String insertCompanyProcess(CompanysVO companyVO, Model model, RedirectAttributes redirectAttributes) {
+	    boolean companyNameCheck = companyService.companyCheckName(companyVO.getCompanyName());
+
+	    if (companyNameCheck) {
+	        redirectAttributes.addFlashAttribute("message", "이미 등록된 회사입니다.");
+	        return "redirect:/company/companyList";  // 리다이렉트 후 메시지 전달
+	    }
+
+	    int companyNo = companyService.insertCompany(companyVO);
+	    return "redirect:/company/companyList";  // 리다이렉트
+	}
+
 
 	// 회사(고객)정보 단건조회(고객명+연락처)
 	@GetMapping("company/companyInfo")
@@ -38,12 +54,14 @@ public class CompanyController {
 		model.addAttribute("companys", list);
 		return "company/companyInfo";
 	}
-
+/*
 	// 회사정보 등록 : URI - insertCompany / RETURN - company/insertCompany
 	@GetMapping("company/insertCompany")
 	public String insertCompanyForm(CompanysVO companyVO) {
 		return "company/insertCompany";
 	}
+	
+	
 
 	// 회사정보 등록 처리
 	@PostMapping("company/insertCompany")
@@ -52,12 +70,12 @@ public class CompanyController {
 
 		if (companyNameCheck) {
 			model.addAttribute("message", "이미 등록된 회사입니다.");
-			return "company/insertCompany";
+			return "company/companyList";
 		}
 		int companyNo = companyService.insertCompany(companyVO);
 		return "redirect:/company/companyList";
 	}
-
+*/
 	// 회사 삭제
 	@ResponseBody
 	@DeleteMapping("company/companyDelete")

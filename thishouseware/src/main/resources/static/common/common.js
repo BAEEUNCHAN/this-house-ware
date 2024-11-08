721e9507f8927a05657c166cb2694c51b3bc3756
fpsgo7 <lkj4769@naver.com>
2024-11-08 14:29:15 +0900

    공통 파트 추가하기
    공통 js 변수 추가
    
    공통 폰트 디자인 추가

---
+++ thishouseware/src/main/resources/static/common/common.js
@@ -0,0 +1,10 @@
+var lang_kor = {
+  "zeroRecords" : "검색된 데이터가 없습니다.",
+  "search" : "",
+  "paginate" : {
+    "first" : "첫 페이지",           
+    "last" : "마지막 페이지",            
+    "next" : "다음",            
+    "previous" : "이전"        
+  },
+}

eb728bccc5d9f3bc14cad614cc3d0e513288c1ba
fpsgo7 <lkj4769@naver.com>
2024-11-13 10:08:06 +0900

    sql 문 업데이트

--- thishouseware/src/main/resources/sql/park/forExampleDate.sql
+++ thishouseware/src/main/resources/sql/park/forExampleDate.sql
@@ -1,30 +1,30 @@
--- 근태 기록 더미데이터로 직원 한명에 대해서만 작동한다.
+-- 근태 기록 더미데이터로 직원 한명에 대해서만 작동한다. (발표전 107 계정에 대하여 실행!)
 DECLARE
 num NUMBER :=1;
 BEGIN
     -- 기존데이터 제거
     DELETE attendances
-    WHERE id = 'emp100';
+    WHERE id = 'emp107';
     LOOP
     -- 출근 정보 추가
     insert into attendances (ATTENDANCES_NO,ATTENDANCES_CODE,DEPARTMENT_NO,TIME,WORKING_TIME,ID) values 
         (attendances_seq.NEXTVAL,'j1',1
         ,TO_DATE('2024-09-01 09:00:00', 'YYYY-MM-DD HH24:MI:SS')
         +num,null
-        ,'emp100');
+        ,'emp107');
     -- 퇴근 정보 추가 (9 ~ 오후 6)
     insert into attendances (ATTENDANCES_NO,ATTENDANCES_CODE,DEPARTMENT_NO,TIME,WORKING_TIME,ID) values 
         (attendances_seq.NEXTVAL,'j2',1
         ,TO_DATE('2024-09-01 18:00:00', 'YYYY-MM-DD HH24:MI:SS')
         +num,540
-        ,'emp100');
+        ,'emp107');
     -- 주말 출퇴근 값 제거
     delete attendances
-    where id='emp100'
+    where id='emp107'
     AND TO_CHAR(time, 'D') in(1,7);
     -- 공휴일 제거 (각각 날짜별로 제거한다.)
     delete attendances
-    where id='emp100'
+    where id='emp107'
     AND TO_CHAR(time, 'MM/DD') in(
     '09/16',
     '09/17',
@@ -41,7 +41,7 @@ BEGIN
     '03/03'
     );
     num := num + 1;
-    EXIT WHEN num > 65;
+    EXIT WHEN num > 72;
     END LOOP;
     commit;
 END;
